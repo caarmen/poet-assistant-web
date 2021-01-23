3ec6ffc8067f613963ef03ac929e6a2bbee6fa86
Carmen Alvarez <c@rmen.ca>
2021-01-23 11:41:33 +0100

    Reorganize code inside MainView.js

--- src/js/view/MainView.js
+++ src/js/view/MainView.js
@@ -1,76 +1,97 @@
 class MainView {
+    static MDCTabBar = mdc.tabBar.MDCTabBar
+    static MDCCircularProgress = mdc.circularProgress.MDCCircularProgress
+    static MDCDialog = mdc.dialog.MDCDialog
+    static MDCTextField = mdc.textField.MDCTextField
+
     constructor() {
-        this.placeholderListRhymes = document.getElementById("placeholder-list-rhymes")
-        this.placeholderListDefinitions = document.getElementById("placeholder-list-definitions")
+        this._elemPlaceholderAppBar = document.querySelector("#placeholder-app-bar")
+        this._elemPlaceholderTabBar = document.querySelector("#placeholder-tab-bar")
+        this._elemPlaceholderProgressIndicator = document.querySelector("#placeholder-progress-indicator")
+        this._elemPlaceholderDialog = document.querySelector("#placeholder-dialog")
+        this._elemPlacholderInputTextSearch = document.querySelector("#placeholder-input-text-search")
+        this._elemPlaceholderBtnSearch = document.querySelector("#placeholder-btn-fetch-definitions")
+        this._elemPlaceholderListRhymes = document.querySelector("#placeholder-list-rhymes")
+        this._elemPlaceholderListDefinitions = document.querySelector("#placeholder-list-definitions")
+
+        this._mdcCircularProgress
+        this._mdcInputTextSearch
+
+        this._elemActionItemAbout
+        this._elemTabRhymer
+        this._elemTabDictionary
+        this._elemBtnSearch
+
         this._template = new Template()
         this._template.loadTemplates().then(() => {
             this.applyTemplates()
-            this.viewModel = new MainViewModel()
+            this.initializeViews()
+            this._viewModel = new MainViewModel()
             this.bindViewModel()
         })
     }
     applyTemplates() {
-        document.querySelector("#placeholder-app-bar").innerHTML = this._template.createAppBarHtml("app-bar", "app_name",
+        this._elemPlaceholderAppBar.innerHTML = this._template.createAppBarHtml("app-bar", "app_name",
             [new AppBarActionItem("action_item_about", "action_item_label_about", "info")])
-        const MDCTopAppBar = mdc.topAppBar.MDCTopAppBar;
-        this.topAppBar = new MDCTopAppBar(document.querySelector("#app-bar"))
-
-        document.querySelector("#action_item_about").onclick = () => { this.viewModel.onAboutClicked() }
-
-        document.querySelector("#placeholder-tab-bar").innerHTML = this._template.createTabBarHtml("tab-bar",
+        this._elemPlaceholderTabBar.innerHTML = this._template.createTabBarHtml("tab-bar",
             [
                 new Tab("tab_rhymer", "tab_rhymer_title"),
                 new Tab("tab_dictionary", "tab_dictionary_title")
             ])
-        const MDCTabBar = mdc.tabBar.MDCTabBar;
-        this.tabBar = new MDCTabBar(document.querySelector(".mdc-tab-bar"))
-        this.tabBar.listen("MDCTabBar:activated", (eventData) => {
+        this._elemPlaceholderProgressIndicator.innerHTML = this._template.createProgressIndicatorHtml()
+        this._elemPlacholderInputTextSearch.innerHTML = this._template.createInputTextHtml("input-text-search", "btn_search_title")
+        this._elemPlaceholderBtnSearch.innerHTML = this._template.createButtonIconHtml("btn-fetch-definitions", "search", "btn_search_title")
+    }
+
+    initializeViews() {
+        var mdcTabBar = new MainView.MDCTabBar(document.querySelector(".mdc-tab-bar"))
+        mdcTabBar.listen("MDCTabBar:activated", (eventData) => {
             this.onTabActivated(eventData["detail"]["index"])
         })
 
-        document.querySelector("#placeholder-progress-indicator").innerHTML = this._template.createProgressIndicatorHtml()
-        const MDCCircularProgress = mdc.circularProgress.MDCCircularProgress;
-        this.circularProgress = new MDCCircularProgress(document.querySelector('.mdc-circular-progress'));
-        this.circularProgress.determinate = false
-        this.circularProgress.open()
+        this._elemActionItemAbout = document.querySelector("#action_item_about")
+        this._elemTabRhymer = document.querySelector("#tab_rhymer")
+        this._elemTabDictionary = document.querySelector("#tab_dictionary")
 
-        document.querySelector("#placeholder-btn-fetch-definitions").innerHTML = this._template.createButtonIconHtml("btn-fetch-definitions", "search", "btn_search_title")
-        this.btnLoad = document.getElementById("btn-fetch-definitions")
-        this.btnLoad.disabled = true
+        this._mdcCircularProgress = new MainView.MDCCircularProgress(document.querySelector('.mdc-circular-progress'))
+        this._mdcCircularProgress.determinate = false
+        this._mdcCircularProgress.open()
 
-        const MDCTextField = mdc.textField.MDCTextField;
-        document.querySelector("#placeholder-input-text-search").innerHTML = this._template.createInputTextHtml("input-text-search", "btn_search_title")
-        this.inputTextSearch = new MDCTextField(document.querySelector("#input-text-search"))
+        this._mdcInputTextSearch = new MainView.MDCTextField(document.querySelector("#input-text-search"))
         document.querySelector("#input-text-search input").onkeydown = (evt) => {
             if (evt.keyCode == 13) this.searchAll()
         }
+
+        this._elemBtnSearch = document.querySelector("#btn-fetch-definitions")
+        this._elemBtnSearch.disabled = true
     }
 
     bindViewModel() {
         // viewmodel -> view bindings
-        this.viewModel.isLoading.observer = (isLoading) => { this.showLoading(isLoading && !this.template.isLoaded) }
-        this.viewModel.rhymes.observer = (newRhymes) => { this.showRhymes(newRhymes) }
-        this.viewModel.definitions.observer = (newDefinitions) => { this.showDefinitions(newDefinitions) }
-        this.viewModel.dialog.observer = (newDialog) => { this.showDialog(newDialog) }
-        this.viewModel.activeTab.observer = (newActiveTab) => { this.switchToTab(newActiveTab) }
+        this._viewModel.isLoading.observer = (isLoading) => { this.showLoading(isLoading && !this.template.isLoaded) }
+        this._viewModel.rhymes.observer = (newRhymes) => { this.showRhymes(newRhymes) }
+        this._viewModel.definitions.observer = (newDefinitions) => { this.showDefinitions(newDefinitions) }
+        this._viewModel.dialog.observer = (newDialog) => { this.showDialog(newDialog) }
+        this._viewModel.activeTab.observer = (newActiveTab) => { this.switchToTab(newActiveTab) }
 
         // view -> viewmodel bindings
-        this.btnLoad.onclick = () => { this.searchAll() }
+        this._elemBtnSearch.onclick = () => { this.searchAll() }
+        this._elemActionItemAbout.onclick = () => { this._viewModel.onAboutClicked() }
     }
     switchToTab(tabIndex) {
-        if (tabIndex == this.viewModel.TAB_RHYMER) {
-            document.querySelector("#tab_rhymer").click()
-        } else if (tabIndex == this.viewModel.TAB_DICTIONARY) {
-            document.querySelector("#tab_dictionary").click()
+        if (tabIndex == this._viewModel.TAB_RHYMER) {
+            this._elemTabRhymer.click()
+        } else if (tabIndex == this._viewModel.TAB_DICTIONARY) {
+            this._elemTabDictionary.click()
         }
     }
     onTabActivated(tabIndex) {
-        if (tabIndex == this.viewModel.TAB_RHYMER) {
-            this.placeholderListRhymes.style.display = "block"
-            this.placeholderListDefinitions.style.display = "none"
-        } else if (tabIndex == this.viewModel.TAB_DICTIONARY) {
-            this.placeholderListRhymes.style.display = "none"
-            this.placeholderListDefinitions.style.display = "block"
+        if (tabIndex == this._viewModel.TAB_RHYMER) {
+            this._elemPlaceholderListRhymes.style.display = "block"
+            this._elemPlaceholderListDefinitions.style.display = "none"
+        } else if (tabIndex == this._viewModel.TAB_DICTIONARY) {
+            this._elemPlaceholderListRhymes.style.display = "none"
+            this._elemPlaceholderListDefinitions.style.display = "block"
         }
     }
     searchAll() {
@@ -78,31 +99,30 @@ class MainView {
         this.searchDefinitions()
     }
     searchRhymes() {
-        this.viewModel.fetchRhymes(this.inputTextSearch.value)
+        this._viewModel.fetchRhymes(this._mdcInputTextSearch.value)
     }
     searchDefinitions() {
-        this.viewModel.fetchDefinitions(this.inputTextSearch.value)
+        this._viewModel.fetchDefinitions(this._mdcInputTextSearch.value)
     }
     showRhymes(rhymes) {
-        this.placeholderListRhymes.innerHTML = this._template.createRhymesListHtml("list-rhymes", rhymes)
+        this._elemPlaceholderListRhymes.innerHTML = this._template.createRhymesListHtml("list-rhymes", rhymes)
     }
     showDefinitions(definitions) {
-        this.placeholderListDefinitions.innerHTML = this._template.createDictionaryListHtml("list-definitions", definitions)
+        this._elemPlaceholderListDefinitions.innerHTML = this._template.createDictionaryListHtml("list-definitions", definitions)
     }
     showLoading(isLoading) {
         if (isLoading) {
-            this.circularProgress.open()
-            this.btnLoad.disabled = true
+            this._mdcCircularProgress.open()
+            this._elemBtnSearch.disabled = true
         } else {
-            this.circularProgress.close()
-            this.btnLoad.disabled = false
+            this._mdcCircularProgress.close()
+            this._elemBtnSearch.disabled = false
         }
     }
     showDialog(newDialog) {
-        document.querySelector("#placeholder-dialog").innerHTML =
+        this._elemPlaceholderDialog.innerHTML =
             this._template.createDialogHtml(newDialog.title, newDialog.content)
-        const MDCDialog = mdc.dialog.MDCDialog;
-        const dialog = new MDCDialog(document.querySelector('.mdc-dialog'))
+        const dialog = new MainView.MDCDialog(document.querySelector('.mdc-dialog'))
         dialog.open()
     }
 }
