6f10dd822881f648d631cd4459175e416e3d319e
Carmen Alvarez <c@rmen.ca>
2021-02-07 21:11:40 +0100

    Avoid opening a new context menu just when the user closed the previous one

--- src/js/view/ContextMenuView.js
+++ src/js/view/ContextMenuView.js
@@ -22,20 +22,36 @@ class ContextMenuView {
         this._elemPlaceholderContextMenu = document.querySelector("#placeholder-context-menu")
         this._template = template
         this.observer = (word, index) => { }
+        this._mdcMenuState = ContextMenuView.MenuState.UNKNOWN
     }
 
     showContextMenu(anchorElement, word, items) {
         this._elemPlaceholderContextMenu.innerHTML = this._template.createContextMenuHtml(items, word)
-        const mdcMenu = new ContextMenuView.MDCMenu(this._elemPlaceholderContextMenu.querySelector(".mdc-menu"))
-        mdcMenu.setAnchorCorner(ContextMenuView.MDCMenuCorner.BOTTOM_LEFT)
-        mdcMenu.setAnchorElement(anchorElement)
-        mdcMenu.setAnchorMargin({ left: 16 })
-        mdcMenu.setFixedPosition(true)
-        mdcMenu.open = true
-        mdcMenu.listen('MDCMenu:selected', (e) => {
-            if (e.detail.index > 0) this.observer(word, e.detail.index - 1)
-        })
+        // Avoid showing another context menu if the user simply clicked away from
+        // the previous context menu to close it
+        if (this._mdcMenuState != ContextMenuView.MenuState.CLOSING) {
+            const mdcMenu = new ContextMenuView.MDCMenu(this._elemPlaceholderContextMenu.querySelector(".mdc-menu"))
+            mdcMenu.setAnchorCorner(ContextMenuView.MDCMenuCorner.BOTTOM_LEFT)
+            mdcMenu.setAnchorElement(anchorElement)
+            mdcMenu.setAnchorMargin({ left: 16 })
+            mdcMenu.setFixedPosition(true)
+            mdcMenu.open = true
+            this._mdcMenuState = ContextMenuView.MenuState.OPENING
+            mdcMenu.listen('MDCMenu:selected', (e) => {
+                if (e.detail.index > 0) this.observer(word, e.detail.index - 1)
+            })
+            mdcMenu.listen('MDCMenuSurface:opened', (e) => {
+                this._mdcMenuState = ContextMenuView.MenuState.OPENED
+            })
+            mdcMenu.listen('MDCMenuSurface:closing', (e) => {
+                this._mdcMenuState = ContextMenuView.MenuState.CLOSING
+            })
+            mdcMenu.listen('MDCMenuSurface:closed', (e) => {
+                this._mdcMenuState = ContextMenuView.MenuState.CLOSED
+            })
+        }
     }
 }
+ContextMenuView.MenuState = Object.freeze({ UNKNOWN: 0, OPENING: 1, OPENED: 2, CLOSING: 3, CLOSED: 4 })
 ContextMenuView.MDCMenu = mdc.menu.MDCMenu
 ContextMenuView.MDCMenuCorner = mdc.menu.Corner
