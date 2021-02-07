85d8c3d57b6fe9228f63244796bdb253df2ca915
Carmen Alvarez <c@rmen.ca>
2021-02-07 17:55:40 +0100

    package linux and mac binaries

--- distribute.sh
+++ distribute.sh
@@ -1,2 +1,6 @@
 npm install electron-packager -g
 electron-packager .  --platform darwin,linux --icon icon.icns --out out
+cd "out/Poet Assistant-darwin-x64"
+zip -r ../PoetAssistant-iOS.zip "Poet Assistant.app"
+cd ..
+tar czf PoetAssistant-linux.tar.gz "Poet Assistant-linux-x64"
