60d9ddd6c180a7e0875a2ac0ca67aaea00b0d66c
Carmen Alvarez <c@rmen.ca>
2021-02-27 18:16:45 +0100

    Have a single source of truth for the app version name
    The single source of truth of the app version name is the version set in `src/i18n/en.json`
    
    Make the `distribute.sh` read the version from `en.json` and use it to:
    * Specify the app version to electron package manager, to override the one in `package.json` (which is now reset to `0.0.0`)
    * Include the version name in the output binary archives

--- distribute.sh
+++ distribute.sh
@@ -1,20 +1,22 @@
 npm install electron-packager -g
 npm install rcedit -g
 
+VERSION=`grep '"app_version": ' src/i18n/en.json | sed -e 's/^.*version":[^"]*"\([0-9\.]*\)".*$/\1/g'` 
+
 echo "Building darwin and linux..."
-electron-packager .  --platform darwin,linux --icon icon.icns --out out
+electron-packager .  --app-version $VERSION --platform darwin,linux --icon icon.icns --out out
 echo "Building win32..."
-electron-packager .  --platform win32 --icon ./favicon.ico --out out
+electron-packager .  --app-version $VERSION --platform win32 --icon ./favicon.ico --out out
 
 echo "Packaging darwin..."
 cd "out/Poet Assistant-darwin-x64"
-tar czf ../PoetAssistant-mac.tar.gz "Poet Assistant.app"
+tar czf ../PoetAssistant-$VERSION-mac.tar.gz "Poet Assistant.app"
 
 echo "Packaging linux..."
 cd ..
 cp ../linux/poet-assistant.bash "Poet Assistant-linux-x64"/
-tar czf PoetAssistant-linux.tar.gz "Poet Assistant-linux-x64"
+tar czf PoetAssistant-$VERSION-linux.tar.gz "Poet Assistant-linux-x64"
 
 echo "Packaging win32..."
-zip -r PoetAssistant-win.zip "Poet Assistant-win32-x64"
+zip -r PoetAssistant-$VERSION-win.zip "Poet Assistant-win32-x64"
 
