d8424c1eee40e2fdbaf98ec98ed4a02fe2de19b2
Carmen Alvarez <c@rmen.ca>
2021-03-07 14:40:14 +0100

    Suggest words from search history

--- src/js/model/suggestions/SuggestionsRepository.js
+++ src/js/model/suggestions/SuggestionsRepository.js
@@ -17,27 +17,60 @@ You should have received a copy of the GNU General Public License
 along with Poet Assistant.  If not, see <http://www.gnu.org/licenses/>.
 */
 class SuggestionsRepository {
-    constructor(db) {
+    constructor(db, settings) {
         this._db = db
+        this._settings = settings
     }
-    async fetchSuggestions(word) {
+    addSearchedWord(word) {
+        this._settings.setSetting(SuggestionsRepository.SETTINGS_KEY_SEARCHED_WORDS,
+            JSON.stringify(
+                Array.from(
+                    this._fetchSearchedWords()
+                        .add(word)
+                        .values()
+                )
+            )
+        )
+    }
+
+
+    async fetchSuggestions(word, includeResultsForEmptyWord) {
+        if (word.length == 0 && !includeResultsForEmptyWord) {
+            return []
+        }
+        return [
+            this._fetchSuggestionsFromHistory(word).map((searchedWord) => new Suggestion(Suggestion.SuggestionType.HISTORY, searchedWord)),
+            (await this._fetchSuggestionsFromDictionary(word)).map((dictionaryWord) => new Suggestion(Suggestion.SuggestionType.DICTIONARY, dictionaryWord))
+        ].flat()
+    }
+
+    _fetchSearchedWords = () => new Set(
+        JSON.parse(
+            this._settings.getSetting(SuggestionsRepository.SETTINGS_KEY_SEARCHED_WORDS, "[]")
+        )
+    )
+
+    _fetchSuggestionsFromHistory = (word) => Array.from(this._fetchSearchedWords().values())
+        .filter((searchedWord) => searchedWord.startsWith(word)).sort()
+
+    async _fetchSuggestionsFromDictionary(word) {
         if (word.length == 0) {
             return []
-        } else {
-            const query = `
+        }
+        const query = `
                 SELECT DISTINCT ${SuggestionsRepository.COL_WORD}
                 FROM ${SuggestionsRepository.TABLE_WORD_VARIANTS}
                 WHERE ${SuggestionsRepository.COL_WORD} LIKE ?
                     AND ${SuggestionsRepository.COL_HAS_DEFINITION}=1
                 ORDER BY ${SuggestionsRepository.COL_WORD}
                 LIMIT ${SuggestionsRepository.LIMIT}`
-            return (await this._db.query(query, [`${word}%`])).map((row) =>
-                row[SuggestionsRepository.COL_WORD]
-            )
-        }
+        return (await this._db.query(query, [`${word}%`])).map((row) =>
+            row[SuggestionsRepository.COL_WORD]
+        )
     }
 }
 SuggestionsRepository.LIMIT = 10
 SuggestionsRepository.TABLE_WORD_VARIANTS = "word_variants"
 SuggestionsRepository.COL_HAS_DEFINITION = "has_definition"
 SuggestionsRepository.COL_WORD = "word"
+SuggestionsRepository.SETTINGS_KEY_SEARCHED_WORDS = "searched_words"
